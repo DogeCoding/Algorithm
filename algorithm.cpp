95f4e19d4f6324dbfcdd150c7b944a46237d77d3
yueng <yangfukai@qiyi.com>
2020-02-27 03:35:54 +0800

    synchronous

--- algorithm.cpp
+++ algorithm.cpp
@@ -36,6 +36,37 @@ struct TreeNode {
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+int maxProfit(vector<int>& prices) {
+	int length = prices.size();
+	int max = 0;
+	for (int i = 0; i < length; i++) {
+		for (int j = i + 1; j < length; j++) {
+			if (prices[j] - prices[i] >  max) {
+				max = prices[j] - prices[i];
+			}
+		}
+	}
+	return max;
+}
+
+int maxProfit(vector<int>& prices) {
+	int minPrice = 0x7fffffff;
+	int maxProfit = 0;
+	for (int i = 0; i < prices.size(); i++) {
+		if (prices[i] < minPrice) {
+			minPrice = prices[i];
+		}
+		if (prices[i] - minPrice > maxProfit) {
+			maxProfit = prices[i] - minPrice;
+		}
+	}
+	return maxProfit;
+}
+
+// 题解
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/solution/121-mai-mai-gu-piao-de-zui-jia-shi-ji-dp-7-xing-ji/
+// 
+
 int main(int argc, char const *argv[])
 {
 	
